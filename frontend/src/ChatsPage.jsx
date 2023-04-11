34674e4303633af6cd54cdc457c3312604a79d6c
Oussama-Fajraoui <oussama.fajraoui1@gmail.com>
2023-04-11 12:46:28 +0100

    adding react chat UI library

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,4 +1,12 @@
+import { MultiChatSocket, MultiChatWindow, useMultiChatLogic} from 'react-chat-engine'
 const ChatsPage = () => {
-    return <div className="background">chats...</div>;
-  };
+    const chatProps = useMultiChatLogic(
+        'ffcd7622-47c4-4ce9-bef8-66eab4681f4f', 
+        props.user.username, 
+        props.user.secret
+    );
+    return <div style={{ height: '100vh'}}>
+        <MultiChatSocket {...chatProps} />
+        <MultiChatWindow {...chatProps} style={{ height: '100%'}} />
+    </div>
   export default ChatsPage;
